74dd6c130d09d34d2a37353ab85c09ead05f9fbf
slynkodenis <slynkodenis@mail.ru>
2020-04-16 11:59:50 +0300

    v1

--- disjoint_set_union/DSU.h
+++ disjoint_set_union/DSU.h
@@ -12,11 +12,35 @@
 #include <vector>
 
 template <typename N>
-struct Node {
+struct Node final {
     size_t rank;
     N data;
     Node *parent;
 
+    Node() : rank(0), data(N()), parent(nullptr) {};
+    Node(const Node& other) : rank(other.rank), data(other.data), parent(nullptr) {};
+    Node(Node&& other) noexcept : rank(other.rank), data(std::move(other.data)), parent(other.parent) {
+        other.parent = nullptr;
+        other.rank = 0;
+    };
+    Node& operator= (const Node& other) {
+        rank = other.rank;
+        data = other.data;
+        parent = nullptr;
+
+        return *this;
+    };
+    Node& operator= (Node&& other) noexcept {
+        rank = other.rank;
+        data = std::move(other.data);
+        parent = other.data;
+
+        other.rank = 0;
+        other.data = nullptr;
+
+        return *this;
+    };
+    ~Node() noexcept = default;
     explicit Node(const N& new_data_) : data(new_data_), rank(0), parent(nullptr) {};
     explicit Node(N&& new_data_) : data(std::move(new_data_)), rank(0), parent(nullptr) {};
     Node *root();
@@ -39,6 +63,28 @@ class DSU final {
     std::vector<Node<N> *> universum;
 
 public:
+    DSU() = default;
+    DSU(const DSU& other) = delete;
+    DSU(DSU&& other) noexcept : universum(other.universum.size()) {
+        for (size_t i = 0, end_ = other.universum.size(); i < end_; ++i) {
+            universum[i] = other.universum[i];
+        }
+
+        other.universum.clear();
+    };
+    DSU& operator= (const DSU& other) = delete;
+    DSU& operator= (DSU&& other) noexcept {
+        for (auto& it : universum)
+            delete it;
+
+        universum = std::move(other.universum);
+
+        return *this;
+    };
+    ~DSU() noexcept {
+        for (auto& it : universum)
+            delete it;
+    };
     explicit DSU(size_t sz) : universum(sz, nullptr) {};
     void reserve(size_t cap) {
         universum.reserve(cap);
@@ -65,6 +111,8 @@ template <typename N>
 bool DSU<N>::equivalent(Node<N> *lhs, Node<N> *rhs) const {
     if (lhs == nullptr || rhs == nullptr)
         throw std::invalid_argument("elements");
+    if (lhs == rhs)
+        return true;
 
     auto lparent = lhs->root();
     auto rparent = rhs->root();
